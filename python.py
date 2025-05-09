99996f69bfa8fd8406be576a9304920c1e61f7ee
Ricardo <rodriguezgutierrezr85@gmail.com>
2025-05-09 17:19:04 -0500

    Add files via upload

---
+++ python.py
@@ -0,0 +1,11 @@
+print("Ingresa la nota del alumno correspondiente: ")
+nota = int(input(":  "))
+
+print("---------PROCESANDO NOTA------------------")
+print("-------------------------------------------")
+
+nombre_Alumno= "Cesar"
+
+
+
+print("La nota del alumno " + nombre_Alumno + " es: " + str(nota))
